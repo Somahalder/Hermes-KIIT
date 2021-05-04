fc3b1d1c4b3dfaefa8be1c1e0239c9119fe8762e
Somahalder <64210798+Somahalder@users.noreply.github.com>
2021-05-05 00:18:42 +0530

    Delete Hermes KIIT directory

--- Hermes KIIT/main.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown() {
-    let navbarToggle = document.getElementById("navbar-toggle");
-    if(navbarToggle.className ===  'main-nav') {
-        navbarToggle.className += ' responsive';
-
-    } else {
-        navbarToggle.className = 'main-nav';
-    }
-}
